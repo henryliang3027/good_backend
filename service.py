129d61da6d990da2ea226a92464cd5c6c721b6a1
henry <henryliang3027@gmail.com>
2026-04-26 08:11:44 +0800

    update bottle and bottle cap detector

--- service.py
+++ service.py
@@ -24,8 +24,8 @@ debug_font = ImageFont.truetype(_FONT_PATH, size=18)
 
 
 # ========== Model & DB Config ==========
-YOLO_MODEL_PATH = "14_bottles_yolo/bottle_detector/best7.pt"
-CAP_YOLO_MODEL_PATH = "caps_yolo/cap_detector/best_L_421_20260413.pt"
+YOLO_MODEL_PATH = "14_bottles_yolo/bottle_detector/best_M_130_20260425.pt"
+CAP_YOLO_MODEL_PATH = "caps_yolo/cap_detector/best_L_101_20260424.pt"
 CAP_CONF_THRESHOLD = 0.80
 BOTTLE_CONF_THRESHOLD = 0.80
 
